5a482b814916fdce3d26c52687896ec0b51b45b1
Góra Bartłomiej <Bartlomiej.Gora@cyfra.gov.pl>
2026-06-29 12:45:30 +0200

    Truck Service

---
+++ trucks/src/main/java/io/github/bartlomiejgora/trucks/TruckService.java
@@ -0,0 +1,4 @@
+package io.github.bartlomiejgora.trucks;
+
+public interface TruckService {
+}
---
+++ trucks/src/main/java/io/github/bartlomiejgora/trucks/TruckServiceImpl.java
@@ -0,0 +1,8 @@
+package io.github.bartlomiejgora.trucks;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+class TruckServiceImpl {
+    private final TruckRepository truckRepository;
+}
