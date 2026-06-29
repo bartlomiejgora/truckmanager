c5ae80f561eb16eeb0b755d2acb221c118346957
Góra Bartłomiej <Bartlomiej.Gora@cyfra.gov.pl>
2026-06-29 14:36:07 +0200

    Truck Service

---
+++ trucks/src/test/java/io/github/bartlomiejgora/trucks/TrucksApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bartlomiejgora.trucks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrucksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
